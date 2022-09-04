3cbd3dce34209e3de5d5ced5abf9cd14805269ad
Noah Cohn <noahc42@gmail.com>
2022-09-04 06:15:34 -0700

    Update infer.py

--- infer.py
+++ infer.py
@@ -64,7 +64,7 @@ class InferenceHelper:
             self.max_depth = 10
             self.saving_factor = 1000
             model = UnetAdaptiveBins.build(n_bins=256, min_val=self.min_depth, max_val=self.max_depth)
-            pretrained_path = "../../pretrained/AdaBins_nyu.pt"
+            pretrained_path = "/content/pretrained/AdaBins_nyu.pt"
         elif dataset == 'kitti':
             self.min_depth = 1e-3
             self.max_depth = 80
