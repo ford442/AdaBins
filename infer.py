84743a844c708d7b026253337360927ee46b2d20
Noah Cohn <noahc42@gmail.com>
2022-09-05 17:55:57 -0700

    Update infer.py

--- infer.py
+++ infer.py
@@ -75,10 +75,10 @@ class InferenceHelper:
         model.eval()
         #self.model = model.to(self.device)
         self.model = model.to(torch.device("cude:0"),non_blocking=True)
-    def ada_CPU(self.model):
-        self.model = model.to(torch.device("cpu"),non_blocking=True)
-    def ada_CUDA(self.model):
-        self.model = model.to(torch.device("cude:0"),non_blocking=True)
+    def ada_CPU():
+        model.to(torch.device("cpu"),non_blocking=True)
+    def ada_CUDA():
+        model.to(torch.device("cude:0"),non_blocking=True)
     @torch.no_grad()
     def predict_pil(self, pil_image, visualized=False):
         img = np.asarray(pil_image) / 255.
