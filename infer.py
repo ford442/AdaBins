17e761cbaaf8e3bb983a2d8773d8ea2b45b0cb7c
Noah Cohn <noahc42@gmail.com>
2022-09-09 21:23:20 -0700

    Update infer.py

--- infer.py
+++ infer.py
@@ -73,7 +73,7 @@ class InferenceHelper:
         model, _, _ = model_io.load_checkpoint(pretrained_path, model)
         model.eval()
         #self.model = model.to(self.device)
-        self.model = model.to(torch.device("cuda:0"),non_blocking=True)
+        self.model = model.to(torch.device("cpu"),non_blocking=True)
     @torch.no_grad()
     def predict_pil(self, pil_image, visualized=False):
         img = np.asarray(pil_image) / 255.
