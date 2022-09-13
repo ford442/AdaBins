751f3ed477cee787b8a3bd67c51e038db81091a3
Noah Cohn <noahc42@gmail.com>
2022-09-13 11:34:45 -0700

    Update model_io.py

--- model_io.py
+++ model_io.py
@@ -1,12 +1,9 @@
 import os
-
 import torch
-
 
 def save_weights(model, filename, path="./saved_models"):
     if not os.path.isdir(path):
         os.makedirs(path)
-
     fpath = os.path.join(path, filename)
     torch.save(model.state_dict(), fpath)
     return
@@ -25,22 +22,19 @@ def save_checkpoint(model, optimizer, epoch, filename, root="./checkpoints"):
         }
         , fpath)
 
-
 def load_weights(model, filename, path="./saved_models"):
     fpath = os.path.join(path, filename)
     state_dict = torch.load(fpath)
     model.load_state_dict(state_dict)
     return model
 
-
 def load_checkpoint(fpath, model, optimizer=None):
-    ckpt = torch.load(fpath, map_location='cpu')
+    ckpt = torch.load(fpath, map_location=None)
     if optimizer is None:
         optimizer = ckpt.get('optimizer', None)
     else:
         optimizer.load_state_dict(ckpt['optimizer'])
     epoch = ckpt['epoch']
-
     if 'model' in ckpt:
         ckpt = ckpt['model']
     load_dict = {}
@@ -50,17 +44,13 @@ def load_checkpoint(fpath, model, optimizer=None):
             load_dict[k_] = v
         else:
             load_dict[k] = v
-
     modified = {}  # backward compatibility to older naming of architecture blocks
     for k, v in load_dict.items():
         if k.startswith('adaptive_bins_layer.embedding_conv.'):
             k_ = k.replace('adaptive_bins_layer.embedding_conv.',
                            'adaptive_bins_layer.conv3x3.')
             modified[k_] = v
-            # del load_dict[k]
-
         elif k.startswith('adaptive_bins_layer.patch_transformer.embedding_encoder'):
-
             k_ = k.replace('adaptive_bins_layer.patch_transformer.embedding_encoder',
                            'adaptive_bins_layer.patch_transformer.embedding_convPxP')
             modified[k_] = v
